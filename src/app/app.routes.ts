46e2f78812e2e7c044cf34e6e918e72a73e56048
udayshankar754 <uday.naxtre@gmail.com>
2024-06-05 13:50:29 +0530

    layout Component Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 export const routes: Routes = [
  
+  {
+    path : '',
+    component : LayoutComponent
+  }
 ];
